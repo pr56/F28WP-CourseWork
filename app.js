7e9a7bf97979e98f84c15597fa8b7c19418eb5af
nethyabanukin <55830568+nethyabanukin@users.noreply.github.com>
2019-11-09 20:08:04 +0400

    Implemented socket.io

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ app.get('/', function(req,res){
 app.use('/Client', express.static(__dirname + '/Client'));
 
 serv.listen(2000);
+console.log("Server started.");
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection',function(socket){
+    
+console.log('socket connection');
+    
+socket.on('test',function(){
+    console.log('test');
+    
+    
+});
+});
