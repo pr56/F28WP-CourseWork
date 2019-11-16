c54ece1a087b52af054427717947204b61de78b9
nethyabanukin <55830568+nethyabanukin@users.noreply.github.com>
2019-11-16 23:37:04 +0400

    Fixed a bug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var connection = mysql.createConnection(
     });
 
     connection.connect(function(err){
-        if (err) {throw err;}
+        if (!err) {throw err;}
 
         console.log('Connected...');
     })
